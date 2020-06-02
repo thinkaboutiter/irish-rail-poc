a6b37766ace58568fe1255b4573320f4db777095
boyankov <boyan.yankov@yahoo.com>
2020-06-02 21:41:56 +0300

    #1 add starter project

---
+++ irish-rail-poc/irish-rail-poc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  irish-rail-poc
+//
+//  Created by Boyan Yankov on 2020-W23-02-Jun-Tue.
+//  Copyright © 2020 boyankov@yahoo.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
