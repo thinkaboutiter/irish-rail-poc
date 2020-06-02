da599292d6b71e46275adfe368728133e0c91826
boyankov <boyan.yankov@yahoo.com>
2020-06-02 22:04:56 +0300

    #1 add `BaseViewController`

---
+++ irish-rail-poc/irish-rail-poc/source/ui-layer/_ui-base-classes/BaseViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.swift
+//  irish-rail-poc
+//
+//  Created by Boyan Yankov on 2020-W23-02-Jun-Tue.
+//  Copyright © 2020 boyankov@yahoo.com. All rights reserved.
+//
+
+import UIKit
+
+/// Abstract.
+/// Base class for all view controllers within the app.
+class BaseViewController: UIViewController {}
