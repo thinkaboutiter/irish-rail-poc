59d1b6f62986e0154813a46601083cfe9c7115f1
boyankov <boyan.yankov@yahoo.com>
2020-06-04 00:08:09 +0300

    #6 add `GetAllStationsWebService`

---
+++ irish-rail-poc/irish-rail-poc/source/networking/web-services/GetAllStationsWebService.swift
@@ -0,0 +1,38 @@
+//
+//  GetAllStationsWebService.swift
+//  irish-rail-poc
+//
+//  Created by Boyan Yankov on 2020-W23-03-Jun-Wed.
+//  Copyright © 2020 boyankov@yahoo.com. All rights reserved.
+//
+
+import Foundation
+import SWXMLHash
+import SimpleLogger
+
+class GetAllStationsWebService: BaseWebService {
+    
+    func getAllStations(success: @escaping (_ stations: [Station]) -> Void,
+                        failure: @escaping (_ error: Swift.Error) -> Void)
+    {
+        super.fetch(success: { (xmlString: String) in
+            do {
+                let result: [Station] = try self.stations(from: xmlString)
+                success(result)
+            }
+            catch {
+                failure(error)
+            }
+        }) { (error: Error) in
+            failure(error)
+        }
+    }
+    
+    private func stations(from xmlString: String) throws -> [Station] {
+        let xmlIndexer: XMLIndexer = SWXMLHash.config { (options: SWXMLHashOptions) in
+            options.shouldProcessLazily = true
+        }.parse(xmlString)
+        let stations: [Station] = try xmlIndexer["ArrayOfObjStation"]["objStation"].value().filter() { $0.latitude != 0.0 && $0.longitude != 0.0}
+        return stations
+    }
+}
