d4db39fd431fa2cfc927dd8b3efeedca7502c9da
boyankov <boyan.yankov@yahoo.com>
2020-06-08 01:32:05 +0300

    #3 update `StationDataCollectionView` add conformance to `CollectionViewDimensionsProvider` protocol

--- irish-rail-poc/irish-rail-poc/source/ui-layer/screens/station/view/_custom-ui/StationDataCollectionView.swift
+++ irish-rail-poc/irish-rail-poc/source/ui-layer/screens/station/view/_custom-ui/StationDataCollectionView.swift
@@ -9,3 +9,53 @@
 import UIKit
 
 class StationDataCollectionView: UICollectionView {}
+
+extension StationDataCollectionView: CollectionViewDimensionsProvider {
+    
+    var paddingLeft: CGFloat {
+        return Dimensions.paddingLeft
+    }
+    
+    var paddingRight: CGFloat {
+        return Dimensions.paddingRight
+    }
+    
+    var minimumInteritemSpacing: CGFloat {
+        return Dimensions.minimumInteritemSpacing
+    }
+    
+    var minimumLineSpacing: CGFloat {
+        return Dimensions.minimumLineSpacing
+    }
+    
+    var itemsPerRow: UInt {
+        return Dimensions.itemsPerRow
+    }
+    
+    var itemWidthToHeightRatio: CGFloat {
+        return Dimensions.itemWidthToHeightRatio
+    }
+    
+    var sectionEdgeInsets: UIEdgeInsets {
+        return Dimensions.sectionEdgeInsets
+    }
+}
+
+// MARK: - Dimensions
+private extension StationDataCollectionView {
+    
+    enum Dimensions {
+        static let paddingLeft: CGFloat = 8
+        static let paddingRight: CGFloat = 8
+        static let minimumInteritemSpacing: CGFloat = 8
+        static let minimumLineSpacing: CGFloat = 8
+        static let itemsPerRow: UInt = (UIDevice.current.userInterfaceIdiom == .phone) ? 2 : 4
+        static let itemWidthToHeightRatio: CGFloat = 4 / 3
+        static var sectionEdgeInsets: UIEdgeInsets {
+            return UIEdgeInsets(top: self.minimumLineSpacing,
+                                left: self.paddingLeft,
+                                bottom: self.minimumLineSpacing,
+                                right: self.paddingRight)
+        }
+    }
+}
