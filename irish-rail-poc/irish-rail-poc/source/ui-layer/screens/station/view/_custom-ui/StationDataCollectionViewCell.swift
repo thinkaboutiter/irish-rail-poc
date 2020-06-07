9c5aa1b5c976700b35857449a05871437d9eb926
boyankov <boyan.yankov@yahoo.com>
2020-06-08 01:48:41 +0300

    #3 add `StationDataCollectionViewCell`

---
+++ irish-rail-poc/irish-rail-poc/source/ui-layer/screens/station/view/_custom-ui/StationDataCollectionViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  StationDataCollectionViewCell.swift
+//  irish-rail-poc
+//
+//  Created by Boyan Yankov on 2020-W24-08-Jun-Mon.
+//  Copyright © 2020 boyankov@yahoo.com. All rights reserved.
+//
+
+import UIKit
+
+class StationDataCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    static var identifier: String {
+        return String(describing: StationDataCollectionViewCell.self)
+    }
+    @IBOutlet private weak var stationDataView: StationDataView!
+    private(set) var stationData: StationData?
+    
+    // MARK: - Life cycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.stationDataView.reset()
+    }
+    
+    // MARK: - Congigurations
+    func configure(with stationData: StationData) {
+        self.stationData = stationData
+        let vm: StationDataViewModel = StationDataViewModelImpl(stationData: stationData)
+        self.stationDataView.configure(with: vm)
+    }
+}
