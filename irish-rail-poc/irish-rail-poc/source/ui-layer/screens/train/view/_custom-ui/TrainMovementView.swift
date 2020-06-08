072a4c9b3d3d89e8d4774856ff9bd73b7e7d1ee4
boyankov <boyan.yankov@yahoo.com>
2020-06-08 23:02:58 +0300

    #4 add ui to `TrainMovementView`

--- irish-rail-poc/irish-rail-poc/source/ui-layer/screens/train/view/_custom-ui/TrainMovementView.swift
+++ irish-rail-poc/irish-rail-poc/source/ui-layer/screens/train/view/_custom-ui/TrainMovementView.swift
@@ -13,6 +13,15 @@ class TrainMovementView: UIView {
     // MARK: - Properties
     // source: https://medium.com/@brianclouser/swift-3-creating-a-custom-view-from-a-xib-ecdfe5b3a960
     @IBOutlet private var contentView: UIView!
+    @IBOutlet private weak var titleContainerView: UIView!
+    @IBOutlet private weak var originDestinationLabel: UILabel!
+    @IBOutlet private weak var dateLabel: UILabel!
+    @IBOutlet private weak var locationLabel: UILabel!
+    @IBOutlet private weak var locationOrderLabel: UILabel!
+    @IBOutlet private weak var arrivalTitleLabel: UILabel!
+    @IBOutlet private weak var arrivalTimeLabel: UILabel!
+    @IBOutlet private weak var departureTitleLabel: UILabel!
+    @IBOutlet private weak var departureTImeLabel: UILabel!
     
     // MARK: - Initialization
     required init?(coder aDecoder: NSCoder) {
