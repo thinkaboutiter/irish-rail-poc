6b7d2d44bbf8b5c8e95586fa8b462fd3ce00ff6e
boyankov <boyan.yankov@yahoo.com>
2020-06-09 00:01:52 +0300

    #4 add ui configurations to `TrainViewControler`

--- irish-rail-poc/irish-rail-poc/source/ui-layer/screens/train/view/TrainViewController.swift
+++ irish-rail-poc/irish-rail-poc/source/ui-layer/screens/train/view/TrainViewController.swift
@@ -18,6 +18,15 @@ class TrainViewController: BaseViewController, TrainViewModelConsumer {
     
     // MARK: - Properties
     private let viewModel: TrainViewModel
+    @IBOutlet weak var trainMovementsCollectionView: TrainMovementsCollectionView! {
+        didSet {
+            let identifier: String = TrainMovementCollectionViewCell.identifier
+            self.trainMovementsCollectionView.register(UINib(nibName: identifier, bundle: nil),
+                                                       forCellWithReuseIdentifier: identifier)
+            self.trainMovementsCollectionView.delegate = self
+            self.trainMovementsCollectionView.dataSource = self
+        }
+    }
     
     // MARK: - Initialization
     @available(*, unavailable, message: "Creating this view controller with `init(coder:)` is unsupported in favor of initializer dependency injection.")
@@ -43,7 +52,7 @@ class TrainViewController: BaseViewController, TrainViewModelConsumer {
     
     // MARK: - TrainViewModelConsumer protocol
     func didFinishFetchingTrainMovements(on viewModel: TrainViewModel) {
-        // TODO: reload data
+        self.trainMovementsCollectionView.reloadData()
     }
     
     func didFailFetchingTrainMovements(on viewModel: TrainViewModel, error: Error) {
@@ -73,5 +82,114 @@ class TrainViewController: BaseViewController, TrainViewModelConsumer {
     
     @objc private func closeButtonTapped(_ sender: UIBarButtonItem) {
         self.navigationController?.dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: - UICollectionViewDataSource protocol
+extension TrainViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int
+    {
+        return self.viewModel.items().count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let identifier: String = TrainMovementCollectionViewCell.identifier
+        guard let validCell: TrainMovementCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as? TrainMovementCollectionViewCell else {
+            let message: String = "Unable to dequeue valid \(identifier)!"
+            fatalError(message)
+        }
+        guard let trainMovement: TrainMovement = self.viewModel.item(at: indexPath) else {
+            let message: String = "Unable to find item for indexPath=\(indexPath)!"
+            Logger.error.message(message)
+            return validCell
+        }
+        validCell.configure(with: trainMovement)
+        return validCell
+    }
+}
+
+// MARK: - UICollectionViewDelegate protocol
+extension TrainViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        didSelectItemAt indexPath: IndexPath)
+    {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout protocol
+extension TrainViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        return self.itemSize(for: collectionView, at: indexPath)
+    }
+    
+    private func itemSize(for collectionView: UICollectionView, at indexPath: IndexPath) -> CGSize {
+        guard let provider: CollectionViewDimensionsProvider = collectionView as? CollectionViewDimensionsProvider else {
+            let message: String = "Unable to obtain valid \(String(describing: CollectionViewDimensionsProvider.self)) object!"
+            debugPrint("❌ \(#file) » \(#function) » \(#line)", message, separator: "\n")
+            return CGSize.zero
+        }
+        let result: CGSize = self.itemSize(for: provider,
+                                           totalWidth: collectionView.bounds.width)
+        return result
+    }
+    
+    private func itemSize(for provider: CollectionViewDimensionsProvider,
+                          totalWidth: CGFloat) -> CGSize
+    {
+        let item_width: CGFloat = (
+            totalWidth
+                - provider.paddingLeft
+                - provider.paddingRight
+                - CGFloat(provider.itemsPerRow - 1) * provider.minimumInteritemSpacing
+            ) / CGFloat(provider.itemsPerRow)
+        
+        let item_height: CGFloat = item_width / provider.itemWidthToHeightRatio
+        return CGSize(width: item_width, height: item_height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets
+    {
+        guard let valid_dimensionsProvider: CollectionViewDimensionsProvider = collectionView as? CollectionViewDimensionsProvider else {
+            let message: String = "Unable to obtain valid \(String(describing: CollectionViewDimensionsProvider.self)) object!"
+            debugPrint("❌ \(#file) » \(#function) » \(#line)", message, separator: "\n")
+            return UIEdgeInsets.zero
+        }
+        return valid_dimensionsProvider.sectionEdgeInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat
+    {
+        guard let valid_dimensionsProvider: CollectionViewDimensionsProvider = collectionView as? CollectionViewDimensionsProvider else {
+            let message: String = "Unable to obtain valid \(String(describing: CollectionViewDimensionsProvider.self)) object!"
+            debugPrint("❌ \(#file) » \(#function) » \(#line)", message, separator: "\n")
+            return 0
+        }
+        return valid_dimensionsProvider.minimumInteritemSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat
+    {
+        guard let valid_dimensionsProvider: CollectionViewDimensionsProvider = collectionView as? CollectionViewDimensionsProvider else {
+            let message: String = "Unable to obtain valid \(String(describing: CollectionViewDimensionsProvider.self)) object!"
+            debugPrint("❌ \(#file) » \(#function) » \(#line)", message, separator: "\n")
+            return 0
+        }
+        return valid_dimensionsProvider.minimumLineSpacing
     }
 }
--- irish-rail-poc/irish-rail-poc/source/ui-layer/screens/train/view/_custom-ui/TrainMovementsCollectionView.swift
+++ irish-rail-poc/irish-rail-poc/source/ui-layer/screens/train/view/_custom-ui/TrainMovementsCollectionView.swift
@@ -9,3 +9,69 @@
 import UIKit
 
 class TrainMovementsCollectionView: UICollectionView {}
+
+extension TrainMovementsCollectionView: CollectionViewDimensionsProvider {
+    
+    var paddingLeft: CGFloat {
+        return Dimensions.paddingLeft
+    }
+    
+    var paddingRight: CGFloat {
+        return Dimensions.paddingRight
+    }
+    
+    var minimumInteritemSpacing: CGFloat {
+        return Dimensions.minimumInteritemSpacing
+    }
+    
+    var minimumLineSpacing: CGFloat {
+        return Dimensions.minimumLineSpacing
+    }
+    
+    var itemsPerRow: UInt {
+        return Dimensions.itemsPerRow
+    }
+    
+    var itemWidthToHeightRatio: CGFloat {
+        return Dimensions.itemWidthToHeightRatio
+    }
+    
+    var sectionEdgeInsets: UIEdgeInsets {
+        return Dimensions.sectionEdgeInsets
+    }
+}
+
+// MARK: - Dimensions
+private extension TrainMovementsCollectionView {
+    
+    enum Dimensions {
+        static let paddingLeft: CGFloat = 8
+        static let paddingRight: CGFloat = 8
+        static let minimumInteritemSpacing: CGFloat = 8
+        static let minimumLineSpacing: CGFloat = 8
+        static var itemsPerRow: UInt {
+            let result: UInt
+            let width: CGFloat = UIScreen.main.bounds.width
+            let height: CGFloat = UIScreen.main.bounds.height
+            if UIDevice.current.userInterfaceIdiom == .phone {
+                if height > width {
+                    result = 2
+                }
+                else {
+                    result = 4
+                }
+            }
+            else {
+                result = 3
+            }
+            return result
+        }
+        static let itemWidthToHeightRatio: CGFloat = 240.0 / 180.0
+        static var sectionEdgeInsets: UIEdgeInsets {
+            return UIEdgeInsets(top: self.minimumLineSpacing,
+                                left: self.paddingLeft,
+                                bottom: self.minimumLineSpacing,
+                                right: self.paddingRight)
+        }
+    }
+}
