c3dc6212181509e73c33c8734a80704d93958ac6
boyankov <boyan.yankov@yahoo.com>
2020-06-08 19:09:04 +0300

    #4 add `Date+Extensions`

---
+++ irish-rail-poc/irish-rail-poc/source/extensions/Date+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Date+Extensions.swift
+//  irish-rail-poc
+//
+//  Created by Boyan Yankov on 2020-W24-08-Jun-Mon.
+//  Copyright © 2020 boyankov@yahoo.com. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    
+    var asRequestQueryStringValue: String {
+        return Self.dateFormatter.string(from: self)
+    }
+    private static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd MMM yyyy"
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }()
+}
