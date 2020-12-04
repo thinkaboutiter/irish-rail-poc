abac6ec9833b53a35519019cfd6e8b372b7b3bac
boyan yankov <thinkaboutiter@gmail.com>
2020-12-04 02:35:50 +0200

    #12 add `RootViewControllerTests` test case subtypes

--- irish-rail-poc/irish-rail-poc-unit-tests/source/ui-layer/screens/root/view/RootViewControllerTests.swift
+++ irish-rail-poc/irish-rail-poc-unit-tests/source/ui-layer/screens/root/view/RootViewControllerTests.swift
@@ -34,27 +34,112 @@ class RootViewControllerTests: XCTestCase {
     }
 }
 
-// MARK: - Mocks
-
+// MARK: - Subtypes
 extension RootViewControllerTests {
     
     private class MockRootViewModel: AbstractMockRootViewModel {
         
-        private(set) var didCall_setViewModelConsumer: Bool = false
+        private(set) var didCall_setViewModelConsumer = false
         
         override func setViewModelConsumer(_ newValue: RootViewModelConsumer) {
             didCall_setViewModelConsumer = true
         }
-        
     }
     
+    // MARK: - Map
     private class MockMapViewControllerFactory: AbstractMockMapViewControllerFactory {
         
-        private(set) var didCall_makeMapViewController: Bool = false
+        private(set) var didCall_makeMapViewController = false
         
         override func makeMapViewController() -> MapViewController {
             didCall_makeMapViewController = true
-            return MapViewController()
+            let result = DummyMapViewController()
+            return result
+        }
+    }
+    
+    private class DummyMapViewController: MapViewController {
+        
+        init() {
+            let viewModel = MockMapViewModel()
+            super.init(
+                viewModel: viewModel,
+                makeStationViewControllerWith: { _ in
+                    return DummyStationViewController()
+                },
+                makeStationsListViewController: {
+                    return DummyStationsListViewController()
+                })
+        }
+    }
+    
+    private class MockMapViewModel: AbstractMockMapViewModel {
+        private(set) var didCall_setViewModelConsumer = false
+        
+        override func setViewModelConsumer(_ newValue: MapViewModelConsumer) {
+            didCall_setViewModelConsumer = true
+        }
+    }
+    
+    // MARK: - Station
+    private class DummyStationViewController: StationViewController {
+        
+        init() {
+            let viewModel = MockStationViewModel()
+            super.init(
+                viewModel: viewModel,
+                makeTrainViewControllerWith: { _ in
+                    return DummyTrainViewController()
+                })
+        }
+    }
+    
+    private class MockStationViewModel: AbstractMockStationViewModel {
+        
+        private(set) var didCall_setViewModelConsumer = false
+        
+        override func setViewModelConsumer(_ newValue: StationViewModelConsumer) {
+            didCall_setViewModelConsumer = true
+        }
+    }
+    
+    // MARK: - Train
+    private class DummyTrainViewController: TrainViewController {
+        
+        init() {
+            let viewModel = MockTrainViewModel()
+            super.init(viewModel: viewModel)
+        }
+    }
+    
+    private class MockTrainViewModel: AbstractMockTrainViewModel {
+        
+        private(set) var didCall_setViewModelConsumer = false
+        
+        override func setViewModelConsumer(_ newValue: TrainViewModelConsumer) {
+            didCall_setViewModelConsumer = true
+        }
+    }
+    
+    // MARK: - StationsList
+    private class DummyStationsListViewController: StationsListViewController {
+        
+        init() {
+            let viewModel = MockStationsListViewModel()
+            super.init(
+                viewModel: viewModel,
+                makeStationViewControllerWith: { _ in
+                    return DummyStationViewController()
+                })
+        }
+    }
+    
+    private class MockStationsListViewModel: AbstractMockStationsListViewModel {
+        
+        private(set) var didCall_setViewModelConsumer = false
+        
+        override func setViewModelConsumer(_ newValue: StationsListViewModelConsumer) {
+            didCall_setViewModelConsumer = true
         }
     }
 }
